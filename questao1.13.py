f3f6bf696cb7575f6a21aea327ea526091c21995
Stephen160623 <136847798+Stephen160623@users.noreply.github.com>
2023-06-30 14:12:13 -0300

    Add files via upload

---
+++ questao1.13.py
@@ -0,0 +1,14 @@
+# Atribuição de variáveis
+   
+# Entrada de dados
+
+r = float(input("Qual a renda do consumidor: "))
+p = float(input("Qual o preço do bem: "))
+
+#Processamento de dados
+
+q = r/p
+
+# Saída de dados
+
+print("A quantidade demandada é: "  + str(q))
